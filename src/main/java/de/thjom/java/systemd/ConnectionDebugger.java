623a672f407452a3004294d3744a7cf03126b9b2
Markus Enax <Markus.Enax@gmail.com>
2017-03-21 21:25:53 +0100

    Add debugger class for connection/disconnection stability testing

---
+++ src/main/java/de/thjom/java/systemd/ConnectionDebugger.java
@@ -0,0 +1,38 @@
+/*
+ * Java-systemd implementation
+ * Copyright (c) 2016 Markus Enax
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of either the GNU Lesser General Public License Version 2 or the
+ * Academic Free Licence Version 2.1.
+ *
+ * Full licence texts are included in the COPYING file with this program.
+ */
+
+package de.thjom.java.systemd;
+
+import org.freedesktop.dbus.DBusConnection;
+import org.freedesktop.dbus.exceptions.DBusException;
+
+public class ConnectionDebugger {
+
+    public static void main(final String[] args) {
+        try {
+            int i = 0;
+
+            while (true) {
+                DBusConnection conn = DBusConnection.getConnection(DBusConnection.SYSTEM);
+
+                System.out.format("ConnectionDebugger.main() - %d - conn=%s\n", i++, conn);
+
+                conn.disconnect();
+
+                Thread.sleep(50);
+            }
+        }
+        catch (final DBusException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
