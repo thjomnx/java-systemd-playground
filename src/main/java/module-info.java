aa92a861c05d75caf8960192a39391187c1016b0
Markus Enax <Markus.Enax@gmail.com>
2021-03-25 21:09:21 +0100

    Remove transitively pulled module dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,4 @@
 module de.thjom.java.systemd.playground {
-	requires org.freedesktop.dbus;
 	requires de.thjom.java.systemd;
 	exports de.thjom.java.systemd.playground.apps;
 	exports de.thjom.java.systemd.playground.devel;
